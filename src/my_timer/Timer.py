7cea590a9e90becd4e9aeb38677a6577e62a3fb4
felipe <felipeteixeira2023@gmail.com>
2026-01-11 17:39:18 -0300

    feat: timer API

--- src/my_timer/Timer.py
+++ src/my_timer/Timer.py
@@ -1,5 +1,41 @@
-from TimeFormat import TimeFormat
+from time import monotonic
 
 class Timer:
-    def __init__(self, time: TimeFormat) -> None:
-        pass
+    """ API to Handle time functions """
+    def __init__(self, duration: float) -> None:
+        self._start = None
+        self._paused_instant = None
+        self._paused_time = 0.0
+        self._duration = duration
+        self._running = False
+
+    def start(self) -> None:
+        """ Starts the timer """
+        self._start = monotonic()
+        self._running = True
+
+    def pause(self) -> None:
+        """ Pauses the timer """
+        self._running = False
+        self._paused_instant = monotonic()
+
+    def progress(self) -> float:
+        """ The progress made compared to the duration in % """
+        return self.elapsed * 100.0 / self._duration
+
+    def resume(self) -> float:
+        """ Resumes the timer """
+        if self._running:
+            return 
+        self._running = True
+        paused_time = monotonic() - self._paused_instant
+        self._paused_time += paused_time
+
+    def elapsed(self) -> tuple[float, float]:
+        """ Time passed since the start point """
+        time_passed = monotonic() - self._start - self._paused_time
+        return time_passed
+    
+    def paused_time(self):
+        """ Returns total paused time """
+        return self._paused_time
