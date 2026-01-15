c3cb007587fe55e9bf7ac1b23f3a0965d238d3a9
felipe <felipeteixeiramello99@gmail.com>
2026-01-15 14:37:25 -0300

    feat: TimeFormat parse and tests

--- src/my_timer/TimeFormat.py
+++ src/my_timer/TimeFormat.py
@@ -1,18 +1,40 @@
 import re
 
 class TimeFormat:
-    """ Validates time formats. Valid format 00h00m00s """
+    """ Validates time formats and returns time in seconds. Valid format 00h00m00s """
     _ALLOWED_FORMAT = re.compile(
             r"""
-            (\d{1,2}h)? # hour
-            (\d{1,2}m)? # minute
-            (\d{1,2}s)? # seconds
+            ^(?=.*\d) # Lookahead so empty strings are invalid
+            (?P<h>\d{1,2}h)? # hour
+            (?P<m>\d{1,2}m)? # minute
+            (?P<s>\d{1,2}s)?$ # seconds
             """
             ,re.I | re.X)
 
     @staticmethod
-    def validate(time_format: str) -> None:
+    def _validate(time_format: str) -> re.Match:
         """ Validates the format if invalid raises ValueError"""
         pattern = TimeFormat._ALLOWED_FORMAT.fullmatch(time_format)
         if not pattern: 
             raise ValueError(f'invalid format, got {time_format}')
+        return pattern
+    
+    @staticmethod
+    def _extract_number(time: str) -> float:
+        """ Extracts a number and returns it """
+        p = re.compile(r'\d{1,2}')
+        return float(p.findall(time)[0])
+
+    @staticmethod
+    def parse(time_format: str) -> float:
+        """ Parses the time format into seconds """
+        s = 0
+        m = TimeFormat._validate(time_format)
+        for t in m.groupdict():
+            if t == 'h' and m[t]:
+                s += TimeFormat._extract_number(m[t]) * 3600
+            elif t == 'm' and m[t]:
+                s += TimeFormat._extract_number(m[t]) * 60
+            elif t == 's' and m[t]:
+                s += TimeFormat._extract_number(m[t])
+        return s
--- tests/timeformat_test.py
+++ tests/timeformat_test.py
@@ -3,17 +3,21 @@ from my_timer.TimeFormat import TimeFormat
 
 class TestTimeFormat:
 
+    def test_empty_string(self):
+        with pytest.raises(ValueError):
+            TimeFormat.parse("")
+
     def test_negative(self):
         with pytest.raises(ValueError):
-            TimeFormat.validate("-1h")
+            TimeFormat.parse("-1h")
 
     def test_minute_input(self):
         with pytest.raises(ValueError):
-            TimeFormat.validate("1h50mm")
+            TimeFormat.parse("1h50mm")
 
     def test_second_input(self):
         with pytest.raises(ValueError):
-            TimeFormat.validate("1h50m50ss")
+            TimeFormat.parse("1h50m50ss")
 
     def test_valid_input(self):
-        assert TimeFormat.validate("1h05m") is None
+        assert TimeFormat.parse("1h00m") == float(3600)
